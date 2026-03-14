347e10e8c17d5f37aefca17643e62fa9e007c611
Johnny <esp25goodlife@gmail.com>
2026-03-14 05:45:09 +0200

    fixed product type

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,17 @@
+import axios from 'axios';
+import type { ProductType } from '../../types/productType';
+import { useEffect, useState } from 'react';
 export const App = () => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get<ProductType[]>(
+        'https://dummyjson.com/products',
+      );
+      setProducts({ ...data });
+    };
+    fetchData();
+  }, []);
+  console.log(products);
   return <></>;
 };
